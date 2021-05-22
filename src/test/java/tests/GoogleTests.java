ad62c8a948e6a452dd21fbd952a91e753badb36b
DeminaDaria <61200143+DeminaDaria@users.noreply.github.com>
2021-05-22 22:54:20 +0300

    update tests

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -5,72 +5,89 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.Calculator;
+import pages.ExpectedResult;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoogleTests {
-    private WebDriver driver;
+    private static WebDriver driver;
+    private static Calculator calculator;
+    private static ExpectedResult expectedResult;
+
 
     @BeforeAll
-    static void setUpAll() {
+    public static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
+        calculator = new Calculator(driver);
+        expectedResult = new ExpectedResult(driver);
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver.get("http://google.com");
     }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldCheckingOperationsWithIntegers() {
-        driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор");
-        driver.findElement(By.cssSelector("div.aajZCb input.gNO89b")).click();
-        driver.findElement(By.cssSelector("div.fB3vD > div")).sendKeys("(1+2)*3-40/5", Keys.ENTER);
+    public void shouldCheckingOperationsWithIntegers() {
 
-        String memoryLine = driver.findElement(By.cssSelector("div.xwgN1d.XSNERd span")).getText();
-        assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", memoryLine);
-        String resultLine = driver.findElement(By.cssSelector("#cwos")).getText();
-        assertEquals("1", resultLine);
+        calculator.search("Калькулятор");
+        calculator.buttonSearch.click();
+        calculator.calculate("(1+2)*3-40/5");
+
+        String memoryLine = expectedResult.getMemoryValue();
+        String resultLine = expectedResult.getResultValue();
+
+        assertAll(
+                () -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", memoryLine),
+                () -> assertEquals("1", resultLine)
+        );
     }
 
     @Test
-    void shouldCheckingDivisionsNull() {
-        driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор");
-        driver.findElement(By.cssSelector("div.aajZCb input.gNO89b")).click();
-        driver.findElement(By.cssSelector("div.fB3vD > div")).sendKeys("6/0", Keys.ENTER);
+    public void shouldCheckingDivisionsNull() {
 
-        String memoryLine = driver.findElement(By.cssSelector("div.xwgN1d.XSNERd span")).getText();
-        assertEquals("6 ÷ 0 =", memoryLine);
-        String resultLine = driver.findElement(By.cssSelector("#cwos")).getText();
-        assertEquals("Infinity", resultLine);
+        calculator.search("Калькулятор");
+        calculator.buttonSearch.click();
+        calculator.calculate("6/0");
+
+        String memoryLine = expectedResult.getMemoryValue();
+        String resultLine = expectedResult.getResultValue();
+
+        assertAll(
+                () -> assertEquals("6 ÷ 0 =", memoryLine),
+                () -> assertEquals("Infinity", resultLine)
+        );
     }
 
     @Test
-    void shouldCheckingNoValue() {
-        driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор");
-        driver.findElement(By.cssSelector("div.aajZCb input.gNO89b")).click();
-        driver.findElement(By.cssSelector("[aria-label=синус]")).click();
-        driver.findElement(By.cssSelector("[aria-label=равно]")).click();
+    public void shouldCheckingNoValue() {
 
-        String memoryLine = driver.findElement(By.xpath("//div[@class='XH1CIc']//span")).getText();
-        assertEquals("sin() =", memoryLine);
-        String resultLine = driver.findElement(By.cssSelector("#cwos")).getText();
-        assertEquals("Error", resultLine);
+        calculator.search("Калькулятор");
+        calculator.buttonSearch.click();
+        calculator.buttonSin.click();
+        calculator.buttonEqual.click();
+
+        String memoryLine = expectedResult.getMemoryValue();
+        String resultLine = expectedResult.getResultValue();
+
+        assertAll(
+                () -> assertEquals("sin() =", memoryLine),
+                () -> assertEquals("Error", resultLine)
+        );
     }
 }
