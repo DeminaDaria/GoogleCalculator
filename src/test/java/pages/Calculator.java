8b693ea5471f9a006fa293efc017daa249d77366
DeminaDaria <61200143+DeminaDaria@users.noreply.github.com>
2021-05-27 20:27:46 +0300

    update Calculator method add cases

--- src/test/java/pages/Calculator.java
+++ src/test/java/pages/Calculator.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,32 +8,80 @@ import static org.openqa.selenium.support.PageFactory.initElements;
 
 public class Calculator {
 
-    @FindBy(css = "input.gLFyf.gsfi")
-    public WebElement searchInput;
-
-    @FindBy(css = "div.aajZCb input.gNO89b")
-    public WebElement buttonSearch;
-
-    @FindBy(css = "div.fB3vD > div")
-    public WebElement calculateValue;
-
     @FindBy(css = "[aria-label=синус]")
     public WebElement buttonSin;
 
     @FindBy(css = "[aria-label=равно]")
     public WebElement buttonEqual;
 
+    @FindBy(css = "[aria-label='открывающая скобка']")
+    public WebElement buttonOpenBrace;
+
+    @FindBy(css = "[aria-label='закрывающая скобка']")
+    public WebElement buttonCloseBrace;
+
+    @FindBy(css = "[aria-label=деление]")
+    public WebElement buttonDivision;
+
+    @FindBy(css = "[aria-label=вычитание]")
+    public WebElement buttonMinus;
+
+    @FindBy(css = "[aria-label=умножение]")
+    public WebElement buttonMultiply;
+
+    @FindBy(css = "[aria-label=сложение]")
+    public WebElement buttonPlus;
+
+    @FindBy(css = "[jsname=bkEvMb]")
+    public WebElement buttonNull;
+
+    @FindBy(css = "[jsname=N10B9]")
+    public WebElement buttonOne;
+
+    @FindBy(css = "[jsname=lVjWed]")
+    public WebElement buttonTwo;
+
+    @FindBy(css = "[jsname=KN1kY]")
+    public WebElement buttonThree;
+
+    @FindBy(css = "[jsname=xAP7E]")
+    public WebElement buttonFour;
+
+    @FindBy(css = "[jsname=Ax5wH]")
+    public WebElement buttonFive;
+
+    @FindBy(css = "[jsname=abcgof]")
+    public WebElement buttonSix;
+
     public Calculator(WebDriver driver) {
         initElements(driver, this);
     }
 
-    public void search(String text) {
-        searchInput.sendKeys(text);
+    public void caseOne() {
+        buttonOpenBrace.click();
+        buttonOne.click();
+        buttonPlus.click();
+        buttonTwo.click();
+        buttonCloseBrace.click();
+        buttonMultiply.click();
+        buttonThree.click();
+        buttonMinus.click();
+        buttonFour.click();
+        buttonNull.click();
+        buttonDivision.click();
+        buttonFive.click();
+        buttonEqual.click();
     }
 
-    public void calculate(String numbers) {
-        calculateValue.sendKeys(numbers, Keys.ENTER);
+    public void caseTwo() {
+        buttonSix.click();
+        buttonDivision.click();
+        buttonNull.click();
+        buttonEqual.click();
     }
 
-
+    public void caseThree() {
+        buttonSin.click();
+        buttonEqual.click();
+    }
 }
